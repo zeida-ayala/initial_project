b6f0477d8db20d87374e3bab04810f478ada61e3
Zeida Ayala Pacheco <zeida_ayala@zayalap-dh01.jalasoft.local>
2016-04-13 11:27:43 -0400

    Initial propject including the gulp tasks

---
+++ build/js/app-870becb40e.js
@@ -0,0 +1,8 @@
+/**
+ * test - test Project Generated from HotTowel Angular
+ * @authors 
+ * @version v0.0.0
+ * @link 
+ * @license 
+ */
+!function(){"use strict";angular.module("app",[])}();
---
+++ src/app.js
@@ -0,0 +1,48 @@
+/*jshint node:true*/
+'use strict';
+
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var port = process.env.PORT || 8001;
+
+var environment = process.env.NODE_ENV;
+
+app.use(favicon(__dirname + '/favicon.ico'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+app.use('/api', require('./routes'));
+
+console.log('About to crank up node');
+console.log('PORT=' + port);
+console.log('NODE_ENV=' + environment);
+
+switch (environment) {
+  case 'build':
+    console.log('** BUILD **');
+    app.use(express.static('./build/'));
+
+    // Any deep link calls should return index.html
+    app.use('/*', express.static('./build/index.html'));
+    break;
+  default:
+    console.log('** DEV **');
+    app.use(express.static('./src/'));
+    app.use(express.static('./'));
+    app.use(express.static('./tmp'));
+
+    // Any deep link calls should return index.html
+    app.use('/*', express.static('./src/index.html'));
+    break;
+}
+
+app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
+  console.log('env = ' + app.get('env') +
+    '\n__dirname = ' + __dirname +
+    '\nprocess.cwd = ' + process.cwd());
+});
